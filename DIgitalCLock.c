9c65841dfe62e4a4f0efefac7977da23e1e9b292
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-05-09 18:52:49 +0530

    Digital clock

---
+++ DIgitalCLock.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+// #include<conio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+	
+	int h,m,s;
+	printf("enter the hour");
+	scanf("%d",&h);
+	printf("enter the minut");
+	scanf("%d",&m);
+	printf("enter the sec");
+	scanf("%d",&s);
+	while(1)
+	{
+			system("clear");
+			
+		printf("%.2d:%.2d:%.2d\n ",h,m,s);
+		
+			s++;
+				
+
+		sleep(1);
+		if(s>59)
+		{
+			s=00;
+			m++;
+		}
+		 if (m>59)
+		{
+			m=00;
+			h++;
+		}
+		if (h>23)
+		{
+			h=00;
+		}
+	
+	
+		
+	}
+}
